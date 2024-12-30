1a1485a51ea5a1c99f84bd079f93a24e4cd4fa63
Manhcodee <phammanh2952@gmail.com>
2024-12-30 16:08:46 +0700

    sua loi class DBConnection va them class Teacher

--- src/main/java/com/example/centermanager/model/Teacher.java
+++ src/main/java/com/example/centermanager/model/Teacher.java
@@ -1,4 +1,57 @@
 package com.example.centermanager.model;
 
 public class Teacher {
+    private int teacherId;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    public Teacher(int teacherId, String fullName, String email, String phoneNumber, String address) {
+        this.teacherId = teacherId;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
--- src/main/java/com/example/centermanager/utils/DBConnection.java
+++ src/main/java/com/example/centermanager/utils/DBConnection.java
@@ -1,4 +1,20 @@
 package com.example.centermanager.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/center_manager";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException("Kết nối đến cơ sở dữ liệu thất bại!", e);
+        }
+    }
 }
